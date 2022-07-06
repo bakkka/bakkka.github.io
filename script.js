1970329d510e435b4bd0c47f1a76229692ab8053
bakkka <91085270+bakkka@users.noreply.github.com>
2022-07-06 12:50:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,40 +26,40 @@ setInterval(function () {
 
 window.onload = function () {
     $.getJSON("https://www.cbr-xml-daily.ru/daily_json.js", function (data) { // Получаем курс валют
-        let s1 = data.Valute.USD.Value; // Получаем Доллар
-        let s2 = data.Valute.EUR.Value; // Получаем Евро
+        let s1 = data.Valute.USD.Value; 
+        let s2 = data.Valute.EUR.Value; 
         let s3 = data.Valute.BYN.Value;
         let s4 = data.Valute.UAH.Value;
         let c = { 'USD': s1, 'EUR': s2, 'RUB': '1', 'BYN': s3, 'UAH': s4 }; // Устанавливаем курс валют
 
-        let val = document.getElementById('val'); // Получаем элемент ввода данных
-        let currency1 = document.getElementById('cur1'); // Получаем первый селект
-        let currency2 = document.getElementById('cur2'); // Получаем второй селект
+        let val = document.getElementById('val'); 
+        let currency1 = document.getElementById('cur1'); 
+        let currency2 = document.getElementById('cur2'); 
         let result = document.getElementsByClassName('output')[0]; // Получаем поле куда будем писать результат
-        function summ() { // Делаем функцию
+        function summ() { 
             let z = 0;
-            if (currency1.value === currency2.value) { // Если оба значения в селектах равны
-                result.innerText = val.value; // То просто вписываем данные из поля ввода
+            if (currency1.value === currency2.value) {
+                result.innerText = val.value; 
             } else {
-                if (currency1.value != 'RUB') { // Если не равны рублю, то
-                    z = val.value * c[currency1.value]; // Переводим сумму в рубли
-                    result.innerHTML = Math.ceil((z / c[currency2.value]) * 100) / 100; // Делим на курс и округляем до сотых
-                } else { // Если не равны
-                    result.innerHTML = Math.ceil((val.value * c[currency2.value]) * 100) / 100; // Умножаем на курс и округляем до сотых
+                if (currency1.value != 'RUB') { 
+                    z = val.value * c[currency1.value]; 
+                    result.innerHTML = Math.ceil((z / c[currency2.value]) * 100) / 100; 
+                } else { 
+                    result.innerHTML = Math.ceil((val.value * c[currency2.value]) * 100) / 100; 
                 }
             }
         }
 
-        val.oninput = function () { // При вводе данных в поле вызываем функцию.
+        val.oninput = function () { 
             summ();
             if (this.value.include('-')) {
                 this.value = '0';
             }
         };
-        currency1.onchange = function () { // При смене первого селекта вызываем функцию.
+        currency1.onchange = function () { 
             summ();
         };
-        currency2.onchange = function () { // При смене второго селекта вызываем функцию.
+        currency2.onchange = function () { 
             summ();
         }
         let USDrate = data.Valute.USD.Value.toFixed(2).replace('.', ',');
